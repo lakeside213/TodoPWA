249b9a15977c2aad7736c547ac8a9ba3e0af272f
lekan <kokulekan23@gmail.com>
2019-03-22 08:01:26 +0100

    Updates

---
+++ precache-manifest.351610c217a05358661cd7424e4900d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1f913f9c3b075b1ce50",
+    "url": "/TodoPWA/static/js/runtime~main.b1f913f9.js"
+  },
+  {
+    "revision": "edff098238253528d4fe",
+    "url": "/TodoPWA/static/js/main.edff0982.chunk.js"
+  },
+  {
+    "revision": "73d2fdfa5ccfd5ff8146",
+    "url": "/TodoPWA/static/js/2.73d2fdfa.chunk.js"
+  },
+  {
+    "revision": "edff098238253528d4fe",
+    "url": "/TodoPWA/static/css/main.664f81b6.chunk.css"
+  },
+  {
+    "revision": "1a62b9db2803cfeedd4e056c9804a15a",
+    "url": "/TodoPWA/index.html"
+  }
+];
